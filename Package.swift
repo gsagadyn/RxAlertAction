e2a2966e4dc94e49e07ceb26364ed4af8ba07d69
Kamil Modzelewski <kammodze@gmail.com>
2021-05-07 01:39:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.0"))
     ],
     targets: [
-        .target(name: "RxAlertAction", dependencies: ["RxSwift", "RxCocoa"], path: "RxAlertAction")
+        .target(name: "RxAlertAction", dependencies: ["RxSwift"], path: "RxAlertAction")
     ]
 )
